d957bc924e259517b00b21f82d85bd13f281207f
nikita0607 <ecfed203@gmail.com>
2021-10-09 00:44:12 +0800

    Some updates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 extern crate reqwest;
 
-use std::io::{stdin, stdout};
+use std::io::{stdin, Read};
+
 use serde_json::Value as JsonValue;
+use serde_json::Error as JsonError;
 
-fn make_respone(addr: &str) -> Result<JsonValue, Box<dyn std::error::Error>> {
 
-    let response_text: std::string::String = client.post(addr)
+fn get_json(data: &str) -> Result<JsonValue, JsonError>{
+
+    let v: JsonValue = serde_json::from_str(data)?;
+
+    Ok(v)
+}
+
+async fn make_response(client: reqwest::Client, addr: &str) -> Result<String, reqwest::Error> {
+
+    println!("{}", addr);
+
+
+    let response_text = client.post(addr)
         .json(&serde_json::json!({
             "computer_name": "Test",
             "user_name": "nikita0607",
-			"adr": "192.168.18.219",
+			"adr": addr,
 			"method": ""
         }))
         .send()
@@ -17,23 +30,25 @@ fn make_respone(addr: &str) -> Result<JsonValue, Box<dyn std::error::Error>> {
         .text()
         .await?;
 
-    serde_json::from_str(&response_text.as_str())?
+    Ok(response_text)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
 
+    println!("Введите часть адреса:");
+
     let mut _addr = String::new();
-    stdin().read_line(&mut _addr);
+    stdin().read_line(&mut _addr)?;
 
-    let addr = std::format!("{}{}{}", "http://192.168.", _addr.as_str(), ":5000/a").as_str();
+    let addr = std::format!("{}{}{}", "http://192.168.", _addr.as_str(), ":5000/a");
 
-    let v: JsonValue = make_respone(&addr).unwrap();
+    let response_text = make_response(client, addr.as_str()).await?;
 
-    let count = v.get("count").unwrap().as_u64().unwrap();
+    let json = get_json(response_text.replace("\n", "").as_str())?;
 
-    println!("{:?}", count);
+    println!("{:?}", json);
 
     Ok(())
 }
