fbe448cb567ce6d66bf9c9a3ce2a176c03cd3034
nikita0607 <ecfed203@gmail.com>
2021-10-08 18:47:58 +0800

    Add features

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-extern crate reqwest
+extern crate reqwest;
 
-use std::collections::HashMap;
+use std::io::{stdin, stdout};
+use serde_json::Value as JsonValue;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
+fn make_respone(addr: &str) -> Result<JsonValue, Box<dyn std::error::Error>> {
 
-    let answer = client.post("http://192.168.20.178:5000/a")
+    let response_text: std::string::String = client.post(addr)
         .json(&serde_json::json!({
             "computer_name": "Test",
             "user_name": "nikita0607",
@@ -15,10 +14,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }))
         .send()
         .await?
-		.json()
-		.await?;
+        .text()
+        .await?;
 
-    println!("{:#?}", answer);
+    serde_json::from_str(&response_text.as_str())?
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let mut _addr = String::new();
+    stdin().read_line(&mut _addr);
+
+    let addr = std::format!("{}{}{}", "http://192.168.", _addr.as_str(), ":5000/a").as_str();
+
+    let v: JsonValue = make_respone(&addr).unwrap();
+
+    let count = v.get("count").unwrap().as_u64().unwrap();
+
+    println!("{:?}", count);
 
     Ok(())
 }
